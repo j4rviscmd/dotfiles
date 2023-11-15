c747f73f2899c46b8bc1eabedf5026a953b714eb
TakurouMaeda <takurou.maeda.developer@gmail.com>
2023-11-15 17:38:00 +0900

    powershellのconfigを追加

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,20 @@
+# Prompt
+Import-Module posh-git
+oh-my-posh init pwsh --config "$env:USERPROFILE\.config\powershell\takurou.omp.json"| Invoke-Expression
+
+Import-Module Terminal-Icons
+
+# Auto suggestions
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView 
+
+
+# Alias
+Set-Alias ll ls
+Set-Alias vim nvim
+Set-Alias grep findstr 
+
+# nvim
+# sample $env:MYENV = "This is my env."
+$env:XDG_CONFIG_HOME = $HOME + "\.config"

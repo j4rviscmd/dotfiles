f272a400522be7e5643bc00a3fd0edbe4b4a120e
Maeda Takurou <takurou0823@gmail.com>
2023-11-16 00:08:27 +0900

    peco コマンドの修正

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -7,30 +7,59 @@ Import-Module Terminal-Icons
 # Auto suggestions
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView 
+Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Emacs
 
 
 # Alias
 Set-Alias ll ls
 Set-Alias vim nvim
-Set-Alias grep findstr 
+Set-Alias grep findstr
 function .. {
     Set-Location ..
 }
 
 # peco
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class KeyboardSimulator
+{
+    [DllImport("user32.dll")]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+    public const int VK_RETURN = 0x0D;
+    public const int KEYEVENTF_KEYUP = 0x0002;
+
+    public static void SimulateKeyEnter()
+    {
+        keybd_event(VK_RETURN, 0, 0, UIntPtr.Zero);
+        keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+    }
+}
+"@
+# press enter key
+# [KeyboardSimulator]::SimulateKeyEnter()
+
 Set-PSReadLineKeyHandler -chord Ctrl+r -scriptBlock { SelectandExecHistory }
 function global:SelectandExecHistory()
 {
-   $selectCmd = (tail -20 (Get-PSReadLineOption).HistorySavePath)|peco --select-1 --on-cancel error
-   if ($?) {
-      [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-      [System.Windows.Forms.SendKeys]::SendWait($selectCmd)
+   $selectCmd = (tail -20 (Get-PSReadLineOption).HistorySavePath) | peco --select-1 --on-cancel error
+   # if ($?) {
+   if (-not $selectCmd) {
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    # return
    } else {
-      [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+    [Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
+     [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selectCmd)
+    # press enter key
+     [KeyboardSimulator]::SimulateKeyEnter()
    }
 }
+
+
 
 # nvim
 # sample $env:MYENV = "This is my env."

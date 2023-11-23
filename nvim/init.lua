b91f5ab4bc88abc3197e8376e4fbe296f9b947d2
Takurou Maeda <takurou.maeda.developer@gmail.com>
2023-11-23 20:36:17 +0900

    UI fix to https://d1pzrilsuqgody.cloudfront.net/?id=sp1jod86dmrdmg4u56h48o39ek

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,18 @@
-require('base')
-require('maps')
-require('plugins')
-require('ui')
+require("takuroumaeda.base")
+require("takuroumaeda.maps")
+require("takuroumaeda.plugins")
+require("takuroumaeda.highlights")
 
 local os = vim.loop.os_uname().sysname
 
-if os == 'Darwin' then 
-  require('macos')
-elseif os == 'Windows_NT' then
-  require('windows')
-elseif os == 'Linux' then
-  require('linux')
+if os == "Darwin" then
+	require("takuroumaeda.macos")
+elseif os == "Windows_NT" then
+	require("takuroumaeda.windows")
+elseif os == "Linux" then
+	require("takuroumaeda.linux")
 else
-  error("Unknown OS")
+	error("Unknown OS")
 end
+
+
--- nvim/lua/maps.lua
+++
@@ -1,6 +0,0 @@
-local keymap = vim.keymap
-
-vim.g.mapleader = "m"
--- keymap.set('n', '<C-o>', ':NERDTreeToggle<CR>')
-keymap.set('n','<C-p>', '<Plug>AirlineSelectPrevTab')
-keymap.set('n','<C-n>', '<Plug>AirlineSelectNextTab')

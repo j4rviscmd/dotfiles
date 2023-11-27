103c15947f6da61bc6f06099273458ec6c293f62
Takurou Maeda <takurou.maeda.developer@gmail.com>
2023-11-27 22:35:22 +0900

    macosにて"com.apple.keylayout.ABC"とすると:q時にエラーが出る問題を解消

--- nvim/after/plugin/im-select.rc.lua
+++ nvim/after/plugin/im-select.rc.lua
@@ -7,7 +7,7 @@ local os = vim.loop.os_uname().sysname
 
 if os == "Darwin" then
 	im_select.setup({
-		default_im_select = "com.apple.keylayout.ABC",
+		default_im_select = "com.apple.keylayout.ABCDEFG",
 		set_previous_events = {},
 	})
 elseif os == "Windows_NT" then

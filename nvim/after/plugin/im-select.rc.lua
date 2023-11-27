d3bd6d27f3479c67faf68f66ae6d514da4d088f9
Maeda Takurou <takurou0823@gmail.com>
2023-11-27 20:26:01 +0900

    im-select(zenhan)にて前回の入力モードを復元しないように修正

--- nvim/after/plugin/im-select.rc.lua
+++ nvim/after/plugin/im-select.rc.lua
@@ -1,24 +1,26 @@
-local status, im_select = pcall(require, 'im_select')
-if ( not status) then return end
+local status, im_select = pcall(require, "im_select")
+if not status then
+	return
+end
 
 local os = vim.loop.os_uname().sysname
 
-if os == 'Darwin' then 
-  im_select.setup {
-    default_im_select = "com.apple.keylayout.ABC"
-  }
-elseif os == 'Windows_NT' then
-  -- English: 0
-  -- Japanese: 1
-  im_select.setup {
-    default_im_select = "0",
-    default_command = 'zenhan',
-    set_default_events = { "VimEnter", "InsertLeave", "CmdlineLeave" },
-    aync_switch_im = true
-  }
-elseif os == 'Linux' then
-  print('Linux does not implement im-select.')
-  return 
+if os == "Darwin" then
+	im_select.setup({
+		default_im_select = "com.apple.keylayout.ABC",
+		set_previous_events = {},
+	})
+elseif os == "Windows_NT" then
+	-- English: 0
+	-- Japanese: 1
+	im_select.setup({
+		default_im_select = "0",
+		default_command = "zenhan",
+		set_previous_events = {},
+	})
+elseif os == "Linux" then
+	print("Linux does not implement im-select.")
+	return
 else
-  return
+	return
 end

cee24d2af5e7e584b0f9fd90ccabfdcc1b6b491f
TakurouMaeda <takurou.maeda.developer@gmail.com>
2024-05-08 11:12:21 +0900

    Update a windows.lua

--- nvim/lua/takuroumaeda/windows.lua
+++ nvim/lua/takuroumaeda/windows.lua
@@ -1,15 +1,16 @@
 -- using windows clipboard with yunk
-vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+-- vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
 
-vim.g.clipboard = {
-  name = 'WslClipboard',
-  copy = {
-    ['+'] = 'win32yank.exe -i --crlf',
-    ['*'] = 'win32yank.exe -i --crlf',
-  },
-  paste = {
-    ['+'] = 'win32yank.exe -o --lf',
-    ['*'] = 'win32yank.exe -o --lf',
-  },
-  cache_enabled = 0,
-}
+-- vim.g.clipboard = {
+--   name = 'WslClipboard',
+--   copy = {
+--     ['+'] = 'win32yank.exe -i --crlf',
+--     ['*'] = 'win32yank.exe -i --crlf',
+--   },
+--   paste = {
+--     ['+'] = 'win32yank.exe -o --lf',
+--     ['*'] = 'win32yank.exe -o --lf',
+--   },
+--   cache_enabled = 0,
+-- }
+vim.opt.clipboard:append({ "unnamedplus" })
